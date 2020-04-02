19b8b54a1f5c2b8ea1b69c371f9ffb7d5597bee3
Amirah Almani <amirahalmani@gmail.com>
2020-04-02 08:47:19 +0300

    hw

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 
+  root 'artists#index'
+  resources :artists, :songs
+  get "bootstrap", to: "artists#bootstrap"
+
 end
